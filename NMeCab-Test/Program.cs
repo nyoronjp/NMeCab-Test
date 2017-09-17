530880ba7fc88cf97fa7262f4ef53e2f9c9d80c0
nyoron <nyoron.jp@live.jp>
2017-09-17 16:04:44 +0900

    first commit

---
+++ NMeCab-Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using NMeCab;
+using System;
+
+namespace NMeCab_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "SOS団には涼宮ハルヒ、キョン、長門有希、朝比奈みくる、古泉一樹の5人がいる。";
+
+            var param = new MeCabParam();
+            param.DicDir = @"./dic/ipadic";
+            param.UserDic = new[] { @"../userdic/haruhi.dic" };
+
+            var mecab = MeCabTagger.Create(param);
+            var node = mecab.ParseToNode(sentence);
+
+            while (node != null)
+            {
+                // if (node.CharType > 0)
+                Console.WriteLine(node.Surface + "\t" + node.Feature);
+
+                node = node.Next;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+﻿# NMeCab-Test
+
+![image](001.png)
+
+## Create user dictionary
+
+```powershell
+mecab-dict-index -d "C:\Program Files (x86)\MeCab\dic\ipadic" -u ./dic/userdic/haruhi.dic -f sjis -t utf-8 .\haruhiDic.csv
+```
+
+## NMeCab
+
+LGPL v2.1
+
+https://ja.osdn.net/projects/nmecab/
+
+## MeCab
+
+http://taku910.github.io/mecab/
+
